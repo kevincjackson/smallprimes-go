6107a9d4b292ff198a72f81f0c5dfafa2fe067fc
Kevin Jackson <kevin@kevincjackson.com>
2022-04-28 21:30:42 -0700

    Adds cli documentation.

--- cmd/smallprimes/smallprimes.go
+++ cmd/smallprimes/smallprimes.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/kevincjackson/smallprimes-go/internal/format"
 	"github.com/kevincjackson/smallprimes-go/pkg/primedata"
@@ -13,30 +14,39 @@ import (
 
 const num_err_msg = "Ooops. Not a number"
 
-var large_num_err_msg string
-
-func init() {
-	large_num_err_msg = fmt.Sprintf("Ooops. Max input is %d.\n", primedata.MaxInt)
-}
+var large_num_err_msg = fmt.Sprintf("Ooops. Max input is %d.\n", primedata.MaxInt)
 
 func main() {
 	var formatArg string
 	app := cli.NewApp()
 	app.Name = "smallprimes"
+	app.Description = "This app does no calculations. It's designed to quickly return primes based on byte data."
+	app.Usage = "get primes up to 10**9."
+	app.UsageText = strings.Join([]string{
+		"smallprimes is 7                   => true",
+		"smallprimes between 7 13           => 7 11 13",
+		"smallprimes upto 7                 => 2 3 5 7",
+		"smallprimes -f json upto 7         => [2, 3, 5, 7]",
+		"smallprimes -f newlines upto 7     => \"2\\n3\\n5\\n7\\n\"",
+		"smallprimes upto 100 > primes.txt  => primes.txt",
+	}, "\n")
+	app.ArgsUsage = "[arrgh]"
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
 			Name:        "format",
 			Aliases:     []string{"f"},
 			Value:       "spaces",
 			Destination: &formatArg,
-			Usage:       "json|newlines|spaces",
+			Usage:       "--format=json|newlines|spaces",
 		},
 	}
 	app.Commands = []*cli.Command{
 		{
 			Name:        "is",
-			Usage:       "is 7",
-			Description: "ask if a number is prime, returns true or false",
+			Usage:       "yields true|false",
+			UsageText:   "smallprimes is 7",
+			Description: "is x prime, yields true|false",
+
 			Action: func(c *cli.Context) error {
 				x, err := strconv.Atoi(c.Args().First())
 				if err != nil {
@@ -51,8 +61,9 @@ func main() {
 		},
 		{
 			Name:        "between",
-			Usage:       "between 100 200",
-			Description: "get all primes beween x and y",
+			Usage:       "yields list of ints",
+			UsageText:   "smallprimes between 10 20",
+			Description: "between x y inclusive, yields list of ints",
 			Action: func(c *cli.Context) error {
 				x, err := strconv.Atoi(c.Args().Get(0))
 				y, err2 := strconv.Atoi(c.Args().Get(1))
@@ -68,8 +79,9 @@ func main() {
 		},
 		{
 			Name:        "upto",
-			Usage:       "upto 100",
-			Description: "get all primes upto x",
+			Usage:       "yields list of ints",
+			UsageText:   "smallprimes upto 10",
+			Description: "upto x inclusive, yields list of ints",
 			Action: func(c *cli.Context) error {
 				x, err := strconv.Atoi(c.Args().Get(0))
 				if err != nil {
