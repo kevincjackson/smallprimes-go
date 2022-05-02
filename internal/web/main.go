7f90c5ab4388299e994bd05f86d4f84191db7666
Kevin Jackson <kevin@kevincjackson.com>
2022-05-02 07:26:34 -0700

    Adds Is page.

--- internal/web/main.go
+++ internal/web/main.go
@@ -1,34 +1,66 @@
 package main
 
 import (
-	"html/template"
-	_ "net/http"
-	"os"
+	"fmt"
+	_ "html/template"
+	"net/http"
+	_ "os"
+	"strconv"
+	"text/template"
+
+	"github.com/kevincjackson/smallprimes-go/pkg/primedata"
 )
 
+const host = "localhost:3001"
+
 func main() {
+	fmt.Printf("Web server runnung on %s\n", host)
 
-	/*
-		type Person struct{ Name string }
-		p1 := Person{"Mr. Jones"}
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/is", isHandler)
 
-		t, err := template.New("test").Parse("Congratulation {{.Name}}. You won a $1,000,000!")
+	http.ListenAndServe(host, nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Server OK")
+}
+
+func isHandler(w http.ResponseWriter, r *http.Request) {
+	var res XData
+	var err error
+	t, _ := template.ParseFiles("is.gohtml")
+	xstr := r.URL.Query().Get("x")
+	if xstr == "" {
+		res = XData{"2", "true", ""}
+	} else {
+		xint, err := strconv.Atoi(xstr)
 		if err != nil {
-			panic(err)
+			res = XData{xstr, "", "Ooops. Input isn't a number."}
+		} else {
+			if xint > primedata.MaxInt {
+				res = XData{xstr, "", "Ooops. Input is too large. "}
+			} else {
+				xres := primedata.Is(xint)
+				res = XData{xstr, strconv.FormatBool(xres), ""}
+			}
 		}
-	*/
-
-	t, _ := template.ParseFiles("hello.gohtml")
-	err := t.Execute(os.Stdout, nil)
+	}
+	err = t.Execute(w, res)
 	if err != nil {
 		panic(err)
 	}
-	/*
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			// fPrintf
-			fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-		})
+}
 
-		http.ListenAndServe(":8080", nil)
-	*/
+type XData struct {
+	X      string
+	Result string
+	Error  string
+}
+
+type XyData struct {
+	X      string
+	Y      string
+	Result string
+	Error  string
 }
